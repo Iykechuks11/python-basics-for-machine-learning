9c687229e901ba708be73e2602721b6191b74c34
iykechuks11 <johnchamp1111@gmail.com>
2024-02-23 13:40:26 +0300

    String methods

--- strings.py
+++ strings.py
@@ -3,15 +3,30 @@ name = 'John'
 age = 28
 
 # Concatenate
-print('Hello, my name is', name, 'and I am', age, 'years old.')
-
-# Or Auguments by position
-print('My name is {name} and I am {age} years old.'.format(name=name, age=age))
-
-# F-Strings (3.6+)
-print(f'Hello, my name is {name} and I am {age} years old')
+# print('Hello, my name is', name, 'and I am', age, 'years old.')
 
 
 # String Formatting
 
+# Or Auguments by position
+# print('My name is {name} and I am {age} years old.'.format(name=name, age=age))
+
+# F-Strings (3.6+)
+# print(f'Hello, my name is {name} and I am {age} years old')
+
+
 # String Methods
+# Capitalization
+# print(name.capitalize())
+
+# Make all uppercase
+# print(name.upper())
+
+# Make all lowercase
+# print(name.lower())
+
+# Swap case - If a letter is upper, it makes it lower, if it is lower it makes it upper, and repeats this for all the letters
+print(name.swapcase())
+
+# Get length
+print(len(name))
